5c6f4cafacb5007907aa5b23bd1e9a07f9e19d5f
Gokhula ramanan <54113936+Gokhularamanan@users.noreply.github.com>
2020-09-10 20:20:13 -0700

    Basic JS completed commit

--- app.js
+++ app.js
@@ -8,3 +8,32 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+var score, activePlayer ,roundScore;
+score = [0,0];
+roundScore = 0;
+
+activePlayer = 1;
+
+
+document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').textContent = 0;
+document.getElementById('score-1').textContent = 0;
+document.getElementById('current-0').textContent = 0;
+document.getElementById('current-1').textContent = 0;
+
+document.querySelector('.btn-roll').addEventListener('click', function ()
+{
+    // Random Number
+    var dice = Math.floor(Math.random() * 6 )+ 1;
+    // Display Result 
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display ='block';
+    diceDOM.src = 'dice-'+ dice + '.png';
+
+});
+
+//document.querySelector('#current-' + activePlayer).textContent = dice;
+//var x = document.querySelector('#current-0').textContent;
+//console.log(x);
